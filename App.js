24758eb74de5b64a26807326f689a884e4c48428
evanvvav <ivankolesov.fi@gmail.com>
2022-03-20 22:14:56 +0200

    Tehty 2.0

--- App.js
+++ App.js
@@ -1,46 +1,58 @@
 import { useEffect, useState } from "react";
-import MapView, { Marker } from 'react-native-maps';
-import { StyleSheet, SafeAreaView, Dimensions, Text, TextInput, Button, } from 'react-native';
+import { StyleSheet, SafeAreaView, Text, Button, TextInput, Image, Picker} from "react-native";
 
 
 export default function App() {
 
-  const [keyword, setKeyword] = useState('');
-  const [latitude, setLatitude] = useState('');
-  const [longitude, setLongitudee] = useState('');
+  const [input, setInput] = useState('');
+  const [exchangeRates, setExchangeRates] = useState([]);
+  const [result, setResult] = useState('');
+  const [currency, setCurrency] = useState("AED");
+  const [pickerItem, setPickerItem] = useState([]);
 
-
-
-  const getRepositories = () => {
-    fetch(`https://www.mapquestapi.com/geocoding/v1/address?key=hEBVB55W9DZZTFs8MHGY1clRvs6TJAcS&location=${keyword.replace(/ /g,'')}`)
-    .then(response => response.json())
-    .then((data) => {
-      setLatitude(data.results[0].locations[0].latLng.lat);
-      setLongitudee(data.results[0].locations[0].latLng.lng);
-
-      });
-    }
   
 
-  return (
+    useEffect(() => {
+    fetch(`http://api.exchangeratesapi.io/v1/latest?access_key=ffb35a4692fdba9c4190856954c31082&format=1`)
+    .then(response => response.json())
+    .then((data) => {
+      setExchangeRates(data.rates);
+      setPickerItem(Object.keys(data.rates));
+    });
+      }, []);
+
+  const getConverted  = () => {
+    setResult(input / exchangeRates[currency].toFixed(2) + "€");
+  };
+
+
+   return (
     <SafeAreaView style={styles.main}>
-      <Text style={styles.text}>Find Adress </Text>
-      <TextInput style={styles.input} onChangeText={ text => setKeyword(text) }/>
-      <Button title="Find" onPress = {getRepositories}/>
-      <MapView  
-        style={styles.map} >
-      <Marker 
-        coordinate={{
-          latitude:latitude, 
-          longitude:longitude}}
-          title='Haaga-Helia' />
-      </MapView>
-    </SafeAreaView>
+    <Image source={{uri: 'https://thumb.mp-farm.com/49913001/preview.jpg'}}
+             style={{width:200, height: 200}} />
+
+      <Text style={styles.text}>Answer: {result} </Text>
+
+      <TextInput style={styles.input} keyboardType="numeric" placeholder="value" onChangeText={ text => setInput(text) } 
+      />
+        <Picker
+          selectedValue={currency}
+          
+          onValueChange={(itemValue) => setCurrency(itemValue)}
+        >
+          {pickerItem.map((item) => (
+            <Picker.Item label={item} value={item} />
+          ))}
+        </Picker>
+      
+      <Button style={styles.button} title="Convert" onPress = {getConverted}/>
+
+      
+    </SafeAreaView> 
   );
 }
 
 const styles = StyleSheet.create({
-
   main: {
     paddingTop: 100,
     flex: 1,
@@ -48,22 +60,23 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: 'white',
   },
-
   text: {
     fontSize: 18,
   },
-  
-  map: {
-    width: Dimensions.get('window').width,
-    height: Dimensions.get('window').height,
-  },
-
   input: {
-    borderBottomWidth: 1,
-    width: 300,
+    borderWidth: 1,
+    width: 160,
     height: 30,
     margin: 1,
     backgroundColor: 'white',
+    padding: 5,
+    
+  },
+  buttons: {
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    width: '40%',
+    margin: 4,
   },
   button: {
     width: 57,
