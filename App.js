94c19d4f7592a70a8d1f232ecbc13e707d9c227b
evanvvav <ivankolesov.fi@gmail.com>
2022-03-20 22:38:52 +0200

    Tehty 2.0

--- App.js
+++ App.js
@@ -1,19 +1,86 @@
-import { StyleSheet } from "react-native";
-import { NavigationContainer } from'@react-navigation/native';
-import { createNativeStackNavigator } from'@react-navigation/native-stack';
-import HomeScreen from "./components/HomeScreen";
-import HistoryScreen from "./components/HistoryScreen";
+import { useEffect, useState } from "react";
+import { StyleSheet, SafeAreaView, Text, View, Button, FlatList, TextInput} from "react-native";
 
 export default function App() {
 
-  const Stack = createNativeStackNavigator();
+  const [result, setResult] = useState(null);
+  const [number1, setNumber1] = useState('');
+  const [number2, setNumber2] = useState('');
+
+  const [history, setHistory] = useState([])
+
+  const handlePlus = () => {
+    setResult(parseInt(number1) + parseInt(number2));
+    setHistory((list) => {
+      return [
+        {text: number1 + ' + ' + number2 + ' = ' + (parseInt(number1) + parseInt(number2)) },
+        ...list
+      ]
+       
+    });
+  }
+
+  const handleMinus = () => {
+    setResult(parseInt(number1) - parseInt(number2));
+    setHistory((list) => {
+      return [
+        {text: number1 + ' - ' + number2 + ' = ' + (parseInt(number1) - parseInt(number2)) },
+        ...list
+      ]
+       
+    });
+  }
 
    return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="History" component={HistoryScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <SafeAreaView style={styles.main}>
+      <Text style={styles.text}>Result: {result}</Text>
+      <TextInput style={styles.input} keyboardType="numeric" 
+      value={number1} onChangeText={(value) => setNumber1(value)}/>
+      <TextInput style={styles.input} keyboardType="numeric" 
+      value={number2} onChangeText={(value) => setNumber2(value)}/>
+    <View style={styles.buttons}>
+      <View style={styles.button}>
+        <Button title="+" onPress={() => handlePlus()}/>
+      </View>
+      <View style={styles.button}>
+        <Button title="-" onPress={() => handleMinus()}/>
+      </View>
+    </View>
+    <Text style={styles.text}>History</Text>
+    <View>
+      <FlatList data={history} renderItem={({ item }) => (
+        <Text style={styles.text}>{item.text}</Text>
+      )}/>
+    </View>
+
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  main: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'silver',
+  },
+  text: {
+    fontSize: 18,
+  },
+  input: {
+    borderWidth: 1,
+    width: 160,
+    height: 30,
+    margin: 1,
+    backgroundColor: 'white',
+  },
+  buttons: {
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    width: '40%',
+    margin: 4,
+  },
+  button: {
+    width: 57,
+  },
+});
