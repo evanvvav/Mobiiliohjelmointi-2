ca5e600df982840477e5c14572ff8dfb52031662
evanvvav <ivankolesov.fi@gmail.com>
2022-03-25 10:30:00 +0200

    1.1

--- App.js
+++ App.js
@@ -1,13 +1,37 @@
 import { useEffect, useState } from "react";
 import { StyleSheet, SafeAreaView, Text, View, Button, FlatList, Image } from "react-native";
+import * as Location from 'expo-location'; 
 
 export default function getRavintolatData() {
 
   const [ravintolat, setRavintolat] = useState([]);
 
-  useEffect(() => {
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+  const [lat, setLat] = useState('');
+  const [lon, setLon] = useState('');
 
-    fetch("https://travel-advisor.p.rapidapi.com/restaurants/list-by-latlng?latitude=60.183832598&longitude=24.942829562&limit=10&currency=USD&distance=2&open_now=false&lunit=km&lang=en_US", {
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+      console.log(location);
+      setLat((location.coords.latitude).toString());
+      setLon((location.coords.longitude).toString());
+
+    })();
+  }, []);
+
+
+  const getRepositories = () => {
+
+    fetch(('https://travel-advisor.p.rapidapi.com/restaurants/list-by-latlng?latitude=' + lat + '&longitude=' + lon + '&limit=30&currency=USD&distance=2&open_now=false&lunit=km&lang=en_US'), {
       "method": "GET",
       "headers": {
         "x-rapidapi-host": "travel-advisor.p.rapidapi.com",
@@ -20,38 +44,44 @@ export default function getRavintolatData() {
       .catch(err => {
         console.error(err);
       });
-  }, []);
+  }
+
+  
 
 
   return (
     <SafeAreaView>
+
+      <Button title="Hanki ravintoloita" onPress={getRepositories} />
+
       <FlatList
-        keyExtractor={(item, index) => index.toString()}
         data={ravintolat}
+        keyExtractor={(item, index) => index.toString()}
+        contentContainerStyle = {{
+          padding: 10,
+        }}
         renderItem={({ item }) => (
-          <View>
-            <Text>{item.name}</Text>
-
-            <Text>{item.address}</Text>
-
-            <Text>{item.address_obj &&
-              item.address_obj.street1}</Text>
-
-            <Text>{item.photo &&
-              item.photo.id}</Text>
-
-            <Text>{item.photo &&
-              item.photo.images.original.url}</Text>
+        
+          <View style={{flexDirection: 'row', padding: 10, marginBottom: 10, backgroundColor: '#ededed', borderRadius: 16, shadowColor: '#000'}}>
 
             <Image source={{
-              uri: item.photo &&
-                item.photo.images.original.url
+              uri: item.photo ?
+                item.photo.images.original.url : 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/60/Pure_black_wallpaper.jpg/640px-Pure_black_wallpaper.jpg'
             }}
-              style={{ width: 200, height: 200 }} />
+              style={{ width: 100, height: 100, marginRight: 10 }} />
 
-            <Text>
+            <View>
 
-            </Text>
+            <Text style={{fontSize: 18}}>{item.name}</Text>
+
+            {item.is_closed ? <Text style={{fontSize: 14}}>Kiini</Text> : <Text style={{fontSize: 14}}>Auki</Text>}  
+
+            <Text style={{fontSize: 14}}>{item.address_obj &&
+              item.address_obj.street1}</Text>
+
+            <Text style={{fontSize: 14}}>{Math.round((item.distance * 1000))}m</Text>
+
+            </View>
 
           </View>
         )}
